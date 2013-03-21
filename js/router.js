1ed817d047ae552db9f9a9cdc8323992d5d03a79
David Eads <davideads@gmail.com>
2013-03-21 14:24:38 -0500

    fix trailing comma

--- js/router.js
+++ js/router.js
@@ -2,7 +2,7 @@ define([ 'backbone' ], function(Backbone) {
     var Router = Backbone.Router.extend({
         routes: {
             '': 'home',
-            'community': 'community_areas',
+            'community': 'community_areas'
         }
     });
     return Router;
