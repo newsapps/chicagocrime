1b47ee325572f285b47e0551dde25941d1f154ee
Ryan Nagle <rnagle@tribune.com>
2013-03-23 15:46:58 -0500

    get sums for the month

--- js/views/CommunityAreaMonthlySummary.js
+++ js/views/CommunityAreaMonthlySummary.js
@@ -39,16 +39,52 @@ function($, Backbone, DateSummaryCollection, PageView, CommunityAreaListView,
                     'crime_date__lt': '2013-' + end + '-01 00:00',
                     'limit': 0
                 },
-                success: _.bind(this.render, this)
+                success: _.bind(function() {
+                    this.prior_year = new DateSummaryCollection();
+                    this.prior_year.fetch({
+                        data: {
+                            'community_area': this.options.community,
+                            'related': 1,
+                            'crime_date__gte': '2012-' + month  + '-01 00:00',
+                            'crime_date__lt': '2012-' + end + '-01 00:00',
+                            'limit': 0
+                        },
+                        success: _.bind(this.render, this)
+                    });
+                }, this)
             });
 
             return this;
         },
 
         render: function() {
-            this.$el.append(this.template());
+            this.$el.append(this.template({
+                month: this.sums(this.summary),
+                prior: this.sums(this.prior_year)
+            }));
 
             $('#content').append(this.$el);
+        },
+
+        sums: function(collection) {
+            var crimes = ["arson", "assault", "battery", "burglary",
+                "criminal_damage", "homicide", "motor_vehicle_theft",
+                "narcotics", "prostitution", "robbery", "sexual_assault",
+                "theft"];
+
+            var sums = {};
+
+            collection.each(function(val, idx) {
+                _.each(crimes, function(i) {
+                    if ( !sums[i] ) {
+                        sums[i] = parseInt(val.get(i), 10);
+                    } else {
+                        sums[i] += parseInt(val.get(i), 10);
+                    }
+                });
+            });
+
+            return sums;
         }
 
     });
